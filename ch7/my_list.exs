7c4c4d6adf73691080cf3fa5a73b1050ba90afd6
Emerson Costa <costtasemerson@gmail.com>
2018-08-26 20:29:16 -0300

    [add] add code for recursive sum exercise

--- ch7/my_list.exs
+++ ch7/my_list.exs
@@ -11,7 +11,11 @@ defmodule MyList do
     def map([], _func), do: []
     def map([head | tail], func), do: [ func.(head) | map(tail, func) ]
 
-    def sum([], total), do: total
-    def sum([head | tail], total), do: sum(tail, total+head)
+    def sum(list), do: _sum(list, 0)
+
+    defp _sum([], total), do: total
+    defp _sum([head | tail], total), do: _sum(tail, total+head)
     
+    def sum2([]), do: 0
+    def sum2([head | tail]), do: head + sum2(tail)
 end
