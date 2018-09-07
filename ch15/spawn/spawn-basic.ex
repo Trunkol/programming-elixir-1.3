9f78ae36b478c44aaa128828cb1616b40bc8c6c2
Emerson Costa <costtasemerson@gmail.com>
2018-09-07 18:53:29 -0300

    [add] add the second version of spawn process

---
+++ ch15/spawn/spawn-basic.ex
@@ -0,0 +1,15 @@
+defmodule SpawnBasic do
+  def greet do
+    receive do
+      {sender, msg} ->
+        send sender, { :ok, "Hello, #{msg}"}
+    end
+  end
+end
+
+pid = spawn(SpawnBasic, :greet, [])
+send pid, {self(), "World"}
+receive do
+  {:ok, message} ->
+    IO.puts message
+end
